ad7ec58de1c114db41e36b55d04604d844e6bdfc
Ieuan Walker <ieuan.walker007@gmail.com>
2022-08-16 15:03:32 +0100

    example

--- Demo/Pages/BasePage.xaml.cs
+++ Demo/Pages/BasePage.xaml.cs
@@ -10,4 +10,30 @@ public partial class BasePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void ThemeToggle_Clicked(object sender, EventArgs e)
+	{
+		if (ThemeToggle == null)
+		{
+			return;
+		}
+
+		switch (ThemeToggle.Text)
+		{
+			case nameof(Theme.Light):
+				Application.Current.Resources = new DarkTheme();
+				ThemeToggle.Text = nameof(Theme.Dark);
+				break;
+
+			default:
+				Application.Current.Resources = new LightTheme();
+				ThemeToggle.Text = nameof(Theme.Light);
+				break;
+		}
+	}
+}
+public enum Theme
+{
+	Light,
+	Dark
 }
--- Demo/Pages/TestPage1.xaml.cs
+++ Demo/Pages/TestPage1.xaml.cs
@@ -1,6 +1,4 @@
-﻿using App.Resources.Styles;
-
-namespace App.Pages;
+﻿namespace App.Pages;
 
 public partial class TestPage1 : BasePage
 {
@@ -13,32 +11,4 @@ public partial class TestPage1 : BasePage
 	{
 		await Navigation.PushAsync(new TestPage2(), true);
 	}
-
-	void ThemeToggle_Clicked(object sender, EventArgs e)
-	{
-		if (ThemeToggle == null)
-		{
-			return;
-		}
-
-		switch (ThemeToggle.Text)
-		{
-			case nameof(Theme.Light):
-				Application.Current.Resources = new DarkTheme();
-				ThemeToggle.Text = nameof(Theme.Dark);
-				break;
-
-			default:
-				Application.Current.Resources = new LightTheme();
-				ThemeToggle.Text = nameof(Theme.Light);
-				break;
-		}
-	}
-
-}
-
-public enum Theme
-{
-	Light,
-	Dark
 }
