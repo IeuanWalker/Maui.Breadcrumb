b85134a313f00d33cb31738aef28d65c6a456fdd
Ieuan Walker <Ieuan.walker007@gmail.com>
2024-04-19 15:30:04 +0100

    Fix: Multipling breadcrumb (#9)
    * Fix bug introduced with .net 8
    * Upgraded project to .net 8

--- Scr/Breadcrumb.xaml.cs
+++ Scr/Breadcrumb.xaml.cs
@@ -144,6 +144,8 @@ public partial class Breadcrumb : ContentView
 
 	async void BreadCrumbContainer_Loaded(object sender, EventArgs e)
 	{
+		BreadCrumbContainer.Loaded -= BreadCrumbContainer_Loaded;
+
 		// Get list of all pages in the NavigationStack that has a selectedPage title
 		List<Page> pages = Navigation.NavigationStack.Select(x => x).Where(x => !string.IsNullOrEmpty(x?.Title)).ToList();
 
