b04996b3d04f62a73d2eb2583323cad62214688e
Ieuan Walker <ieuan.walker007@gmail.com>
2024-11-19 18:38:22 +0000

    Refactor Alert method to access main page from first window
    Updated the Alert method in TestViewModel to retrieve the main
    page from the first window of the current application instead
    of directly from the MainPage property. Changed variable name
    from mainpage to mainPage to reflect this new approach.

---
+++ Demo/ViewModels/TestViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace App.ViewModels;
+
+[INotifyPropertyChanged]
+public partial class TestViewModel
+{
+	[RelayCommand]
+	static async Task Clicked()
+	{
+		await Alert("Clicked command", "Button clicked");
+	}
+
+	[RelayCommand]
+	static async Task Pressed()
+	{
+		await Alert("Pressed command", "Button pressed");
+	}
+
+	[RelayCommand]
+	static async Task Released()
+	{
+		await Alert("Released command", "Button released");
+	}
+
+	static async Task Alert(string title, string message)
+	{
+		Page? mainPage = Application.Current?.Windows[0].Page;
+		if (mainPage is not null)
+		{
+			await mainPage.DisplayAlert(title, message, "OK");
+		}
+	}
+}
