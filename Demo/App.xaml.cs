eba906c948f51a03ba37f1b9843c4a14b59ca144
Ieuan Walker <ieuan.walker007@gmail.com>
2024-11-19 18:36:58 +0000

    Refactor main page setup to use CreateWindow method
    Updated App.xaml.cs to override CreateWindow method for setting
    up the main page. Replaced direct MainPage assignment in the
    constructor with a NavigationPage containing TestPage1. This
    change aligns with new requirements for window creation and
    lifecycle management.

--- Demo/App.xaml.cs
+++ Demo/App.xaml.cs
@@ -7,7 +7,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new NavigationPage(new TestPage1());
 	}
+
+	protected override Window CreateWindow(IActivationState? activationState) => new(new NavigationPage(new TestPage1()));
 }
